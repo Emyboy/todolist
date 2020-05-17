cc9284f894924f9d0096a7a292ed6913ea37970b
Emyboy <emyboybeats@gmail.com>
2020-05-17 09:44:26 -0700

    formated code

--- DOM/CRUD/index.js
+++ DOM/CRUD/index.js
@@ -1,7 +1,8 @@
-let dummyData = [];
+let todoListArray = [];
+
 const todoComponent = (title, id) => {
     return `<div id='todo-${id}' class="shadow alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>${id}: ${title}</strong>
+  <strong>${id + 1}: ${title}</strong>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span delete-id='${id}' onClick='removeTodo(${id})' id='delete-btn' aria-hidden="true">&times;</span>
   </button>
@@ -9,30 +10,29 @@ const todoComponent = (title, id) => {
 };
 
 let inputValue;
-document.getElementById('text-input').addEventListener('input', e => inputValue = e.target.value);
-
+document.getElementById('text-input').addEventListener('input', e => {
+    inputValue = e.target.value
+});
 
 const renderToDos = () => {
     document.getElementById('todo-list').innerHTML += "";
-    dummyData.map((val, i) => {
+    todoListArray.map((val, i) => {
         val === "" ? null : document.getElementById('todo-list').innerHTML += todoComponent(val.title, val.id)
     })
 };
 
 const removeTodo = (id) => {
-    console.log(dummyData, id);
+    todoListArray.splice(id,1);
     document.getElementById(`todo-${id}`).remove();
-    dummyData.map((val, i) => dummyData[id] = '');
-    console.log(dummyData)
-    // dummyData.splice(id, 1);
+    todoListArray.map((val, i) => todoListArray[id] = '');
+    todoListArray.length === 0 ? document.getElementById('todo-list').innerHTML = `<h4>No Todo Add Yet</h4>` : null;
 }
 
 const createToDO = (todo) => {
-    console.log('adding', todo);
-    if(todo.title === undefined || todo.title === ""){
+    if (todo.title === undefined || todo.title === "") {
         alert('Please enter text');
-    }else {
-        dummyData.push({ title: todo.title, id: dummyData.length });
+    } else {
+        todoListArray.push({ title: todo.title, id: todoListArray.length });
         document.getElementById('todo-list').innerHTML = '';
         renderToDos();
         document.getElementById('text-input').value = '';
@@ -46,8 +46,11 @@ document.querySelectorAll('#delete-btn').forEach((val, id) => {
     })
 });
 
-document.getElementById('add-btn').addEventListener('click', () => createToDO({ title: inputValue }))
+document.getElementById('add-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    createToDO({ title: inputValue });
+})
 
 
 // adding placeholder text 
-dummyData.length === 0 ? document.getElementById('todo-list').innerHTML = `<h4>No Todo Add Yet</h4>` : null;
+todoListArray.length === 0 ? document.getElementById('todo-list').innerHTML = `<h4>No Todo Add Yet</h4>` : null;
